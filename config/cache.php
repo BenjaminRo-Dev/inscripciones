48b1b62f3498fcfc2793955883cfc510e25af2a3
Benjamin <programador.ben@gmail.com>
2025-10-16 11:55:14 -0400

    refactor: :recycle: aumentado el tiempo de cache

--- config/cache.php
+++ config/cache.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Str;
 
 return [
 
-    'tiempo_cache' => env('TIEMPO_CACHE', 60), //Tiempo en segundos que se guarda en cache el resultado de la operacion
-    'tiempo_cache_error' => env('TIEMPO_CACHE_ERROR', 300), // tiempo en segundos que se guarda en cache el error de la operacion
+    'tiempo_cache' => env('TIEMPO_CACHE', 600), //Tiempo en segundos que se guarda en cache el resultado de la operacion
+    'tiempo_cache_error' => env('TIEMPO_CACHE_ERROR', 600), // tiempo en segundos que se guarda en cache el error de la operacion
 
     /*
     |--------------------------------------------------------------------------
